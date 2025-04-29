f210fb38bcad4033816f0e510fb3b977f2d756a5
MD Siyam Bhuiyan <siyam35-702@diu.edu.bd>
2025-04-29 14:47:55 +0600

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -41,6 +43,9 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
         
 # The above code defines a data ingestion class that reads a CSV file, splits it into training and testing sets, and saves them to specified paths. It uses logging for tracking the process and handles exceptions with a custom exception class. The `DataIngestionConfig` dataclass is used to define the configuration for data ingestion, including paths for the train, test, and raw data files.
